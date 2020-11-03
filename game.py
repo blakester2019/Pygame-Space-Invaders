37a24bc6414b32ce102c5c7ee75c2b2043c6c2c7
Blake Rhodes <blakester2019@gmail.com>
2020-11-02 19:53:32 -0600

    added enemies

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ class Laser(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.Surface((40, 10))
         self.image.fill((255,0,0))
-        self.rect = self.image.get_rect(center = (Xi, Yi))
+        self.rect = self.image.get_rect(center = (Xi, Yi + 50))
 
     def update(self):
         self.rect.x += 10
@@ -40,10 +40,27 @@ class Morty(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x -= 10
-        if self.rect.x < WIDTH/random.randint(2, 6):
+        if self.rect.x < WIDTH/random.randint(4, 8):
             if self.children == 0:
                 self.children = 1
                 morty_group.add(Morty())
+
+        if self.rect.x < 0:
+            self.kill()
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Alien.png")
+        self.rect = self.image.get_rect(center = (WIDTH, random.randint(50, 500)))
+        self.children = 0
+
+    def update(self):
+        self.rect.x -= 10
+        if self.rect.x < WIDTH/random.randint(2, 6):
+            if self.children < 1:
+                self.children += 1
+                morty_group.add(Alien())
 
         if self.rect.x < 0:
             self.kill()
@@ -66,6 +83,11 @@ laser_group = pygame.sprite.Group()
 
 # Morty
 morty_group = pygame.sprite.Group()
+morty_group.add(Morty())
+
+# Aliens
+alien_group = pygame.sprite.Group()
+alien_group.add(Alien())
 
 while True:
     for event in pygame.event.get():
@@ -79,20 +101,24 @@ while True:
                 rick.pressed_keys["down"] = True
             if event.key == pygame.K_SPACE:
                 laser_group.add(rick.shoot_laser())
-                morty_group.add(Morty())
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
                 rick.pressed_keys["up"] = False
             if event.key == pygame.K_s:
                 rick.pressed_keys["down"] = False
 
+    #pygame.sprite.groupcollide(laser_group, alien_group, True, True):
+    
+
     # Draw Objects
     screen.fill((30, 30, 30))
     laser_group.draw(screen)
     rick_group.draw(screen)
     morty_group.draw(screen)
+    alien_group.draw(screen)
     rick_group.update()
     laser_group.update()
     morty_group.update()
+    alien_group.update()
     pygame.display.flip()
     clock.tick(30)
